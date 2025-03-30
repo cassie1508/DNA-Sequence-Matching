d6e745a40dde1886117d2806946413f2eab978b5
amandaagambire <ada007@bucknell.edu>
2025-03-30 15:30:58 -0400

    working LCS

--- L_C_Sequence.py
+++ L_C_Sequence.py
@@ -1,25 +1,30 @@
 def longest_common_sequence(X, Y):
     m = len(X)
     n = len(Y)
-    LCS = [[0 for i in range(m + 1)] for j in range(n + 1)]
-    print(LCS)
-    
-    for i in range(1, m+1):  
-        # print("i-1", i-1)
-        for j in range(1, n+1):  
-            #print("j-1", j-1)
-            # print (X[i-1], Y[j-1])
-            if X[i-1] == Y[j-1]:
-                print("i", i, "j", j)
-                print (LCS[j][i])
+    LCS = [[0 for i in range(n + 1)] for j in range(m + 1)]
+ 
+    for i in range(m+1): 
+        for j in range(n+1):  
+            if i== 0 or  j == 0:
+                LCS[i][j] = 0
+            elif X[i-1] == Y[j-1]:
                 LCS[i][j] = 1 + LCS[i-1][j-1]
-
-        #         #if the characters are the same, add 1 to the previous diagonal value
             else:
-                print("i-1", i-1 , "j", j)
                 LCS[i][j] = max(LCS[i-1][j], LCS[i][j-1])
+    i, j = m, n
+    lcs_items = []
+    while i > 0 and j > 0:
+        if X[i-1] == Y[j-1]:
+            lcs_items.append(X[i-1])
+            i -= 1
+            j -= 1
+        elif LCS[i-1][j] > LCS[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+    lcs_items.reverse()
+    lcs_string = ''.join(lcs_items)
 
-    print(LCS)
-    return LCS
+    return lcs_string
 
-longest_common_sequence("AGGTAB", "GXTXAYB")
+print (longest_common_sequence("ACADB", "CBDA" ))
